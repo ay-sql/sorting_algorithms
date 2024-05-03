d78f2bdbee0d878751580e973f931286975c22ba
ay-sql <e.sqli.ayoub@gmail.com>
2024-05-04 01:09:17 +0200

    test

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,43 +1,46 @@
 #include "sort.h"
 
 /**
- * swap_elements - Swap two elements in an array.
- * @a: Pointer to the first element.
- * @b: Pointer to the second element.
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
-void swap_elements(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using
- *                  the selection sort algorithm.
- * @array: Pointer to the array to be sorted.
- * @size: Size of the array.
+ * selection_sort - Sort an array of integers in ascending order
+ *                  using the selection sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
  *
  * Description: Prints the array after each swap.
  */
 void selection_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	int *min;
+	size_t i, j;
 
-    size_t i, j;
-    int *min;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        min = array + i;
-        for (j = i + 1; j < size; j++)
-            min = (array[j] < *min) ? (array + j) : min;
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
 
-        if ((array + i) != min)
-        {
-            swap_elements(array + i, min);
-            print_array(array, size);
-        }
-    }
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
 }
+
